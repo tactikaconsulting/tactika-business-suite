630c35d577947659c1b3fdd65a69d61870c4cbf9
claudio andres urra irarrazabal <claudioandresurrairarrazabal@MacBook-Air-de-claudio.local>
2026-07-18 12:54:00 -0400

    Fix TopBar import case

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,4 @@
-export { default as Header } from "./Header";
-export { default as Sidebar } from "./Sidebar";
-export { default as Topbar } from "./Topbar";
-export { default as SidebarItem } from "./SidebarItem";
 export { default as Logo } from "./Logo";
+export { default as Sidebar } from "./Sidebar";
+export { default as SidebarItem } from "./SidebarItem";
+export { default as TopBar } from "./TopBar";
